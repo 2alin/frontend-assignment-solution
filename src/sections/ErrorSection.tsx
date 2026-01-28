242569435093041f1fa059d73ef244250856e5ae
Adilson Sandoval <adilsonsdev@gmail.com>
2026-01-28 21:50:41 +0100

    fix alignment detail

--- src/sections/ErrorSection.tsx
+++ src/sections/ErrorSection.tsx
@@ -1,6 +1,6 @@
 export default function ErrorSection() {
   return (
-    <section className="flex flex-1 flex-col items-center justify-center p-2 font-semibold text-secondary-600">
+    <section className="flex flex-1 flex-col items-center justify-center p-2 font-semibold text-secondary-600 text-center">
       <header className="mb-2 flex flex-col items-center gap-2 text-lg font-bold text-secondary-800">
         <span className="size-20 bg-primary-500 mask-[url(./assets/cloud-alert.svg)] mask-size-[100%]"></span>
         <h1>Something went wrong</h1>
